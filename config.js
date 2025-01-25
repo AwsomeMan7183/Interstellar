edea46b8e29e0a3f20983c3bb51c63bd4c122fcd
AwsomeMan7183 <146497477+AwsomeMan7183@users.noreply.github.com>
2025-01-24 20:25:45 -0500

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ const config = {
   challenge: true, // Set to true if you want to enable password protection.
   users: {
     // You can add multiple users by doing username: 'password'.
-    process.env.USER_1: "process.env.PASS_1",
-    process.env.USER_2: "process.env.PASS_2"
+    admin: "zayloveparkersomuch",
+    test: "test"
   },
 };
 
